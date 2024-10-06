ea7726fc42d3bdfc2d2bf815ffc89c2f8e497217
ZanosCosmos <92492530+ZanosCosmos@users.noreply.github.com>
2024-10-06 13:07:07 -0400

    Break-Break

--- Ashen Cosmos.ltproj/resources/custom_components/custom_skill_components.py
+++ Ashen Cosmos.ltproj/resources/custom_components/custom_skill_components.py
@@ -17,3 +17,56 @@ class DoNothing(SkillComponent):
 
     expose = ComponentType.Int
     value = 1
+
+class LostOnEndNextCombat(SkillComponent):
+    nid = 'lost_on_end_next_combat'
+    desc = "Remove after subsequent combat"
+    tag = SkillTags.TIME
+
+    expose = (ComponentType.MultipleOptions)
+
+    value = [["NumberOfCombats (X)", "2", 'Number of combats before expiration'],["LostOnSelf (T/F)", "T", 'Lost after self combat (e.g. vulnerary)'],["LostOnAlly (T/F)", "T", 'Lost after combat with an ally'],["LostOnEnemy (T/F)", "T", 'Lost after combat with an enemy'],["LostOnSplash (T/F)", "T", 'Lost after combat if using an AOE item']]
+    
+    def init(self, skill):
+        self.skill.data['combats'] = self.values.get('NumberOfCombats (X)', '2')
+
+    @property
+    def values(self) -> Dict[str, str]:
+        return {value[0]: value[1] for value in self.value}
+
+    def post_combat(self, playback, unit, item, target, mode):
+        from app.engine import skill_system
+        remove_skill = False
+        if self.values.get('LostOnSelf (T/F)', 'T') == 'T':
+            if unit == target:
+                val = int(self.skill.data['combats']) - 1
+                action.do(action.SetObjData(self.skill, 'combats', val))
+                if int(self.skill.data['combats']) <= 0:
+                    remove_skill = True
+
+        if self.values.get('LostOnAlly (T/F)', 'T') == 'T':
+            if target:
+                if skill_system.check_ally(unit, target):
+                    val = int(self.skill.data['combats']) - 1
+                    action.do(action.SetObjData(self.skill, 'combats', val))
+                    if int(self.skill.data['combats']) <= 0:
+                        remove_skill = True
+        if self.values.get('LostOnEnemy (T/F)', 'T') == 'T':
+            if target:
+                if skill_system.check_enemy(unit, target):
+                    val = int(self.skill.data['combats']) - 1
+                    action.do(action.SetObjData(self.skill, 'combats', val))
+                    if int(self.skill.data['combats']) <= 0:
+                        remove_skill = True
+        if self.values.get('LostOnSplash (T/F)', 'T') == 'T':
+            if not target:
+                val = int(self.skill.data['combats']) - 1
+                action.do(action.SetObjData(self.skill, 'combats', val))
+                if int(self.skill.data['combats']) <= 0:
+                    remove_skill = True
+
+        if remove_skill:
+            action.do(action.RemoveSkill(unit, self.skill))
+
+    def on_end_chapter(self, unit, skill):
+        action.do(action.RemoveSkill(unit, self.skill))
